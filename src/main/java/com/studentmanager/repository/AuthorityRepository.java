f3823f4492ba049867c369cf042e915c796c073e
tudao888 <daotu2106@gmail.com>
2023-05-08 10:08:55 +0700

    Initial version of studentManager generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/studentmanager/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.studentmanager.repository;
+
+import com.studentmanager.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/studentmanager/service/InvalidPasswordException.java
@@ -0,0 +1,10 @@
+package com.studentmanager.service;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidPasswordException() {
+        super("Incorrect password");
+    }
+}
